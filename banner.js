260113af3ce4fa43bed374b64383eb9c59c0bfa6
Evansy <72968116+evansyxd@users.noreply.github.com>
2021-07-29 16:17:34 +0300

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,5 +1,5 @@
         const user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
-        let uid = user.id // let uid = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author; XXX
+        let uid = user.id // let uid = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author; = x !
         let response = fetch(`https://discord.com/api/v8/users/${uid}`, {
             method: 'GET',
             headers: {
